f5402a1d5a6ec7aded2803afecc10eda260bdc43
xujunxian1993 <416409548@qq.com>
2018-02-28 16:37:07 +0800

    Initial commit

---
+++ src/main/java/com/yuqiyu/less2/Less2Application.java
@@ -0,0 +1,12 @@
+package com.yuqiyu.less2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Less2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Less2Application.class, args);
+	}
+}
---
+++ src/test/java/com/yuqiyu/less2/Less2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yuqiyu.less2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Less2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
